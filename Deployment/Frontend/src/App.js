fe0a88a743363fa45d0ffe317bf54da629151aeb
shashwat0704 <90603161+shashwat0704@users.noreply.github.com>
2024-05-02 11:56:27 +0530

    Add files via upload

---
+++ Deployment/Frontend/src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import './App.css';
+import BasicTextFields from './components/Fields';
+
+function App() {
+  return (
+    <div className="App">
+
+      <BasicTextFields>
+
+      </BasicTextFields>
+    </div>
+  );
+}
+
+export default App;
